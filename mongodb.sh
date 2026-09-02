06bc30355a0458c0e29fbfc4996b4ac41488a0cd
Noor-devops <noorshanu07@gmail.com>
2026-09-02 13:26:17 +0530

    shell

--- mongodb.sh
+++ mongodb.sh
@@ -7,17 +7,21 @@ sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
 sudo chmod -R 755 $LOGS_FOLDER
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ $USERID -ne 0 ]; then
-    echo "$TIMESTAMP Please run this script with root access"
+    echo -e "$TIMESTAMP $R Please run this script with root access $N"
     exit 1
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "$TIMESTAMP [ERROR] $2 is..... FAILURE" | tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP [ERROR] $2 is..... $R FAILURE $N" | tee -a $LOGS_FILE
     else
-        echo "$TIMESTAMP [INFO] $2 is..... SUCCESS" | tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP [INFO] $2 is..... %G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
 cp mongo.repo /etc/yum.repos.d/mongo.repo
