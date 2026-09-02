af8f964b8928c881e976ba2394758119010396a3
Noor-devops <noorshanu07@gmail.com>
2026-09-02 13:17:21 +0530

    shell

--- mongodb.sh
+++ mongodb.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script with root access"
+    echo " $TIMESTAMP Please run this script with root access"
     exit 1
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo " $2 is..... FAILURE"
+        echo " $TIMESTAMP [ERROR] $2 is..... FAILURE"
     else
-        echo " $2 is..... SUCCESS"
+        echo " $TIMESTAMP [INFO] $2 is..... SUCCESS"
     fi
 }
 cp mongo.repo /etc/yum.repos.d/mongo.repo
