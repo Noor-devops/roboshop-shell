a248f6a0fa8c1d2b8f7c3ab0d1f107c4d50a9cee
Noor-devops <noorshanu07@gmail.com>
2026-09-02 13:08:34 +0530

    shell

---
+++ mongodb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root access"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo " $2 is..... FAILURE"
+    else
+        echo " $2 is..... SUCCESS"
+}
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo repo"
