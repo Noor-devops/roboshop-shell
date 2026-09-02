f58858af17e2a4b0f300bf1704f481c0b637fc0c
Noor-devops <noorshanu07@gmail.com>
2026-09-02 13:21:17 +0530

    shell

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 USERID=$(id -u)
+sudo mkdir -p $LOGS_FOLDER
+sudo chown -r ec2-user:ec2-user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER
+LOGS_FILE="$LOGS_FOLDER/$0.log"
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 if [ $USERID -ne 0 ]; then
@@ -10,13 +14,13 @@ fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo " $TIMESTAMP [ERROR] $2 is..... FAILURE"
+        echo " $TIMESTAMP [ERROR] $2 is..... FAILURE" | tee -a $LOGS_FILE
     else
-        echo " $TIMESTAMP [INFO] $2 is..... SUCCESS"
+        echo " $TIMESTAMP [INFO] $2 is..... SUCCESS" | tee -a $LOGS_FILE
     fi
 }
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Adding Mongo repo"
 
-dnf install mongodb-org -y 
+dnf install mongodb-org -y &>> $LOGS_FILE
 VALIDATE $? "Installing mongo"
