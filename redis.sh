0402bc170a8439ec400125ad59cdb4ec94e74547
Noor-devops <noorshanu07@gmail.com>
2026-09-04 23:23:34 +0530

    shell

--- redis.sh
+++ redis.sh
@@ -31,7 +31,7 @@ dnf install redis -y &>> $LOGS_FILE
 VALIDATE $? "Installing redis:7"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-VALIDATE $? "Allowing remote connections to MongoDB"
+VALIDATE $? "Allowing remote connections to Redis"
 
 systemctl enable redis &>> $LOGS_FILE
 systemctl start redis  &>> $LOGS_FILE
