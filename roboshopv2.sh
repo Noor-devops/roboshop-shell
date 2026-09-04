d1a7749a7572d164c8494095b79c2aef693f8c4b
Noor-devops <noorshanu07@gmail.com>
2026-09-04 14:58:26 +0530

    shell

---
+++ roboshopv2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z07090442QTQZUF01CVZY"
+DOMAIN_NAME="nirfaws.online"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+###validation###
+if [ $# -lt 2 ]; then
+    echo -e "$R ERROR: Atleast 2 arguments required $N"
+    echo "USAGE: $0 [create/delete] [instance1] [instance2...]"
+    exit 1
+fi
