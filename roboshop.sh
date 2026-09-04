113831c787ec10f8d3cfd4412a84576f04e53590
Noor-devops <noorshanu07@gmail.com>
2026-09-04 11:10:58 +0530

    shell

--- roboshop.sh
+++ roboshop.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do 
     echo "Launching the Instance: $instance "
+    INSTANCE_ID=$(aws ec2 run-instances \
+        --image-id ami-0220d79f3f480ecf5 \
+        --instance-type t3.micro \
+        --security-groups "roboshop-common" \ 
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
+        --query 'Instances[0].InstanceId' \
+        --output text
+    )
 done
