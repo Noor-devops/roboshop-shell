c84a2c5986aef77ca3d576191ba81caacca0eae0
Noor-devops <noorshanu07@gmail.com>
2026-09-05 16:04:04 +0530

    shell

---
+++ 03-mysql.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/roboshop"
+sudo mkdir -p $LOGS_FOLDER
+sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$TIMESTAMP $R Please run this script with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$TIMESTAMP [ERROR] $2 is..... $R FAILURE $N" | tee -a $LOGS_FILE
+    else
+        echo -e "$TIMESTAMP [INFO] $2 is..... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf install mysql-server -y &>> $LOGS_FILE
+VALIDATE $? "Installing Mysql server"
+
+systemctl enable mysqld &>> $LOGS_FILE
+systemctl start mysqld  &>> $LOGS_FILE
+VALIDATE $? "Enabled and start Mysql server"
+
+mysql_secure_installation --set-root-pass RoboShop@1 &>> $LOGS_FILE
+VALIDATE $? "Setting up root password"
+
--- roboshop.sh
+++ roboshop.sh
@@ -9,8 +9,8 @@ do
     INSTANCE_ID=$(aws ec2 run-instances \
         --image-id $AMI_ID \
         --instance-type t3.micro \
-        --security-groups "roboshop-common" "roboshop-$instance" \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
+        --security-groups "roboshop-common" "expense-$instance" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=expense-$instance}]" \
         --query 'Instances[0].InstanceId' \
         --output text
     )
